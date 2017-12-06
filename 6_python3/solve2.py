6dce1f92a2cd96ba4ac2eb0572b7b70a557e78d9
Agnieszka Bielec <agnieszka.bielec@cert.pl>
2017-12-06 16:32:59 +0100

    day 6

---
+++ 6_python3/solve2.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+def read(fname):
+    with open(fname,"r") as f:
+        return f.read()
+
+
+banks =  list(map(int, read("input.txt").split()))
+print (banks)
+
+
+d = dict()
+redistributions = 0
+
+l = len(banks)
+
+#go
+
+loop = None
+
+while True:
+    tbanks = tuple(banks)
+    if tbanks in d:
+        loop = redistributions - d[tbanks]
+        break
+    d[tbanks] = redistributions
+
+    #redistribute
+    m = max(banks)
+    mi = banks.index(m)
+    
+    banks[mi] = 0
+    
+    #optimization
+    full = m // l
+    rest = m % l
+    
+    for i in range(l):
+        banks[i] += full
+        
+    m = rest
+    #end of optimization
+    
+    i = (mi + 1) % l
+    while m!=0:
+        banks[i] += 1
+        m -= 1
+        i = (i + 1) % l
+    
+    redistributions += 1
+
+print ("redistributions to find a loop - %d" % redistributions)
+print ("loop size - %d" % loop)
